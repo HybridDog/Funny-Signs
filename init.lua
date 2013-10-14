4df77c071329524cd9bf2d4e0a9042e2e533a427
TheggaB77 <na-cho1@live.com>
2013-10-14 01:21:29 -0300

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,332 @@
+minetest.register_node("funnysigns:fartzone", {
+	description = "Fart Zone Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"fartzone.png"},
+	wield_image =  "fartzone.png",
+	inventory_image =  "fartzone.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:toilet", {
+	description = "Toilet Zone Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"toilet.png"},
+	wield_image =  "toilet.png",
+	inventory_image =  "toilet.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:aliens", {
+	description = "Aliens Zone Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"aliens.png"},
+	wield_image =  "aliens.png",
+	inventory_image =  "aliens.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:parac", {
+	description = "Parachuting Bears Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"parac.png"},
+	wield_image =  "parac.png",
+	inventory_image =  "parac.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:partyzone", {
+	description = "Party Zone Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"partyzone.png"},
+	wield_image =  "partyzone.png",
+	inventory_image =  "partyzone.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:menat", {
+	description = "Men at Work Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"menat.png"},
+	wield_image =  "menat.png",
+	inventory_image =  "menat.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+ 
+minetest.register_node("funnysigns:norat", {
+	description = "No Rat Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"norat.png"},
+	wield_image =  "norat.png",
+	inventory_image =  "norat.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+
+minetest.register_node("funnysigns:accid", {
+	description = "Accident Zone Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"accid.png"},
+	wield_image =  "accid.png",
+	inventory_image =  "accid.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:bird", {
+	description = "Bird Poop Zone Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"bird.png"},
+	wield_image =  "bird.png",
+	inventory_image =  "bird.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:cowpo", {
+	description = "No Cow Poop Zone Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"cowpo.png"},
+	wield_image =  "cowpo.png",
+	inventory_image =  "cowpo.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:notco", {
+	description = "You are Not Cool Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"notco.png"},
+	wield_image =  "notco.png",
+	inventory_image =  "notco.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:poop", {
+	description = "Poop Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"poop.png"},
+	wield_image =  "poop.png",
+	inventory_image =  "poop.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:sec", {
+	description = "Fence Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"sec.png"},
+	wield_image =  "sec.png",
+	inventory_image =  "sec.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:sparta", {
+	description = "This is Sparta Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"sparta.png"},
+	wield_image =  "sparta.png",
+	inventory_image =  "sparta.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:donoten", {
+	description = "Do not Enter Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"donoten.png"},
+	wield_image =  "donoten.png",
+	inventory_image =  "donoten.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:elev", {
+	description = "Elevator Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"elev.png"},
+	wield_image =  "elev.png",
+	inventory_image =  "elev.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:child", {
+	description = "Fire Kill Children Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"child.png"},
+	wield_image =  "child.png",
+	inventory_image =  "child.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:dontsa", {
+	description = "Don't Say Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"dontsa.png"},
+	wield_image =  "dontsa.png",
+	inventory_image =  "dontsa.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:astmar", {
+	description = "Aston Martin Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"astmar.png"},
+	wield_image =  "astmar.png",
+	inventory_image =  "astmar.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:mine", {
+	description = "Mine Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"mine.png"},
+	wield_image =  "mine.png",
+	inventory_image =  "mine.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:run", {
+	description = "Run Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"run.png"},
+	wield_image =  "run.png",
+	inventory_image =  "run.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+minetest.register_node("funnysigns:surf", {
+	description = "Surfaces Sign",
+	drawtype = "signlike",
+	walkable = false,
+	tiles = {"surf.png"},
+	wield_image =  "surf.png",
+	inventory_image =  "surf.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	selection_box = {
+		type = "wallmounted",
+	},
+	groups = {oddly_breakable_by_hand = 3}, 
+});
+
+print("FUNNYSIGNS INIT OK")
